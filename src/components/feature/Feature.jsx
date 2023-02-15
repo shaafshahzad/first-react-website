b124beb8223da1bd57c05d1047ba18b57463db34
Shaaf <shaaf.m.shahzad@gmail.com>
2023-02-15 02:23:31 -0500

    Added feature component for about container

---
+++ src/components/feature/Feature.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './feature.css'
+
+const Feature = ( {title, text} ) => {
+  return (
+    <div className="features-container_feature">
+      <div className="features-container_feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="features-container_feature-text">
+          <p>{text}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Feature
