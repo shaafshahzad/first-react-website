fc9e11cbd63bf82e05c13f30147a062b7b58052e
Shaaf <shaaf.m.shahzad@gmail.com>
2023-02-20 12:56:22 -0500

    Styled and formatted the CTA component

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,8 +3,14 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>
-      CTA
+    <div className="cta">
+      <div className="cta-content">
+        <p>Come see what everyone else is talking about</p>
+        <h3>Sign up today & discover what there is to offer.</h3>
+      </div>
+      <div className="cta-btn">
+        <button type='button'>Get Started</button>
+      </div>
     </div>
   )
 }
